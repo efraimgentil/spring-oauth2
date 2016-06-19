bfed591d33521c20604cf8b7d54e7f7756b6e2b5
root <efraimgentil@gmail.com>
2016-06-19 14:38:06 -0300

    Working on client1

--- client1/app/app.js
+++ client1/app/app.js
@@ -1,23 +1,25 @@
-var app = angular.module('myApp', ["ngResource","ngRoute","ngCookies" ,"oauth"]);
-app.config(function($locationProvider) {
-  $locationProvider.html5Mode({
-      enabled: true,
-      requireBase: false
+var app = angular.module('myApp', ["ngResource", "ngRoute", "ngCookies", "oauth"]);
+app.constant("$userResourceUrl", "http://localhost:8080/ws");
+
+app.config(function ($locationProvider) {
+    $locationProvider.html5Mode({
+        enabled: true,
+        requireBase: false
     }).hashPrefix('!');
 });
 app.factory('oauthHttpInterceptor', function (Storage) {
-  return {
-    request: function (config) {
-      // This is just example logic, you could check the URL (for example)
-      var token = Storage.get("token");
-      if ( token ) {
-        config.headers.Authorization = 'Bearer ' + token.access_token;
-      }
-      return config;
-    }
-  };
+    return {
+        request: function (config) {
+            // This is just example logic, you could check the URL (for example)
+            var token = Storage.get("token");
+            if (token) {
+                config.headers.Authorization = 'Bearer ' + token.access_token;
+            }
+            return config;
+        }
+    };
 });
-app.factory('responseObserver', function ($q , $window) {
+app.factory('responseObserver', function ($q, $window) {
     return {
         responseError: function (errorResponse) {
             switch (errorResponse.status) {
@@ -36,28 +38,47 @@ app.config(function ($httpProvider) {
     $httpProvider.interceptors.push('oauthHttpInterceptor');
     $httpProvider.interceptors.push('responseObserver');
 });
-app.service("usuarioService" , [ "$http" , "Storage"] , function($http , Storage){
-    this.getUserPermissions = function(){
+app.service("usuarioService", ["$http", "Storage", "$userResourceUrl", function ($http, Storage, $userResourceUrl) {
+    this.getUserPermissions = function (callback) {
         var token = Storage.get("token");
-        if(token){
-
-        }else{
-            return [];
+        if (token) {
+            $http.get($userResourceUrl + "/user-permissions").then(function success(response) {
+                callback(response.data);
+            });
+        } else {
+            callback([]);
         }
     }
+    this.isUserAuthenticated = function () {
+        var token = Storage.get("token");
+        return (token) ? true : false;
+    }
+}]);
 
 
-});
+app.controller('mainCtrl', ["$scope", "$resource", "$http", "usuarioService",
+    function ($scope, $resource, $http, usuarioService) {
+        var self = this;
+        $scope.$on('oauth:login', function(event, token) { self.loadPermissions(); });
+        $scope.$on('oauth:authorized', function(event, token) { self.loadPermissions(); });
+
+        self.loadPermissions = function(){
+            $scope.permissions = {};
+            usuarioService.getUserPermissions(function (data) {
+                for(var i = 0 ; i < data.length ; i++ ){
+                    $scope.permissions[data[i]] = true;
+                }
+                console.log( $scope.permissions );
+            });
+            $scope.userAuthenticated = usuarioService.isUserAuthenticated();
+        };
+        self.loadPermissions();
 
 
-var http;
-app.controller('mainCtrl', function($scope,$resource, $http) {
-    http =  $http;
-
-    $scope.foo = {id:0 , name:"sample foo"};
-    $scope.foos = $resource( "http://localhost:9080/hey");
-    $scope.getFoo = function(){
-        $scope.foo = $scope.foos.get();
-        console.log( $scope.foo );
-    } 
-});
+        $scope.foo = {id: 0, name: "sample foo"};
+        $scope.foos = $resource("http://localhost:9080/hey");
+        $scope.getFoo = function () {
+            $scope.foo = $scope.foos.get();
+            console.log($scope.foo);
+        }
+    }]);
